22abc98967278f565bf910a545217a9a1ad59894
amirul@posable.com.my <Magnet46!@>
2024-11-01 18:28:53 +0800

    remove static data and add dynamic timedate format

--- tests/mail.spec.js
+++ tests/mail.spec.js
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
 const { MainPage } = require('../pageobjects/MainPage');
-const dataSet = JSON.parse(JSON.stringify(require("../utils/testData.json")));
+
+function generateDynamicString() {
+  const now = new Date();
+  return `test_${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}_${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
+}
 
 test.describe( () => {
   test('Verify mailbox', async ({ page }) => {
     const mainPage = new MainPage(page);
+    const dynamicMailbox = generateDynamicString();
     await mainPage.goTo();
-    await mainPage.addMail(dataSet.mailbox);
-    await expect(page.locator('h2')).toContainText(dataSet.mailbox);
+    await mainPage.addMail(dynamicMailbox);
+    await expect(page.locator('h2')).toContainText(dynamicMailbox);
   });
 })
